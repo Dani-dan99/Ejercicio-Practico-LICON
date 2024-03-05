23c1296d9cc5700e594d6d07aaff300b7f3e25fd
Luisa Daniela <ldcpdan@gmail.com>
2024-03-05 10:19:59 -0600

    Estructura Nextjs

---
+++ src/app/category/[id]/page.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-anonymous-default-export, react/display-name
+export default function () {
+    return (
+        <div>
+            <h1>Card Page</h1>
+        </div>
+    )
+};
